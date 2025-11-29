0d7486ec7551a18c3b29a5af6ce143b8365b43aa
sukjin123 <tjrwls5661@naver.com>
2025-11-29 23:43:59 +0900

    Initial commit

---
+++ src/main/java/com/example/emotiondiary/EmotiondiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.emotiondiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotiondiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotiondiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/emotiondiary/EmotiondiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emotiondiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotiondiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
